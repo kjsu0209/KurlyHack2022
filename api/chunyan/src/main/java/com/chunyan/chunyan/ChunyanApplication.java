a7988848de765490772230cf8e489535fc1e393a
jsooOO <35682236+kjsu0209@users.noreply.github.com>
2022-08-20 02:52:41 +0900

    add api template

---
+++ api/chunyan/src/main/java/com/chunyan/chunyan/ChunyanApplication.java
@@ -0,0 +1,13 @@
+package com.chunyan.chunyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChunyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChunyanApplication.class, args);
+	}
+
+}
---
+++ api/chunyan/src/test/java/com/chunyan/chunyan/ChunyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunyan.chunyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChunyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
